124358d307894464ae6ea1def5793b9f62099392
Aman Rao <amanrao032@gmail.com>
2024-01-16 19:04:35 +0530

    added read and write

--- scull/scull.c
+++ scull/scull.c
@@ -13,7 +13,8 @@ MODULE_LICENSE("Dual BSD/GPL");
 static int scull_major = 0;
 static int scull_minor = 0;
 static int scull_nr_devs = 4;
-static int scull_qset = 1000;
+static int scull_qset_val = 1000;
+static int scull_quantum_val = 4000;
 
 module_param(scull_major, int, S_IRUGO);
 
@@ -28,6 +29,10 @@ struct scull_dev{
 	struct cdev cdev;
 	struct scull_qset *data;
 	int qset;
+	unsigned long size;
+	int quantum;
+	unsigned int access_key;
+	struct semaphore sem;
 };
 
 int scull_trim(struct scull_dev *dev){
@@ -47,7 +52,9 @@ int scull_trim(struct scull_dev *dev){
 	}
 
 	dev->data = NULL;
-	dev->qset = scull_qset;
+	dev->qset = scull_qset_val;
+	dev->size = 0;
+	dev->quantum = scull_quantum_val;
 	return 0;
 }
 
@@ -71,17 +78,135 @@ int scull_release(struct inode * inode, struct file * filp){
 	return 0;
 }
 
+struct scull_qset* scull_follow(struct scull_dev *dev, int item){
+	struct scull_qset* ptr = dev->data;
+	struct scull_qset* rptr = NULL;
+
+	for(int i=0; i<item+1; i++){
+		pr_info("DEBUG: ptr = %p, ptr->next = %p\n", ptr, ptr ? ptr->next : NULL);
+		if(ptr == NULL){
+			ptr = kmalloc(sizeof(struct scull_qset), GFP_KERNEL);
+			if (ptr == NULL)
+				return NULL;
+			memset(ptr, 0, sizeof(struct scull_qset));
+			ptr->data = NULL;
+			ptr->next = NULL;
+		}
+
+		rptr = ptr;
+		ptr = ptr->next;
+	}
+
+	return rptr;
+}
+
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos){
+	ssize_t retval = 0;
+	struct scull_dev *dev = filp->private_data;
+	struct scull_qset *dptr;
+	int quantum = dev->quantum, qset = dev->qset;
+	int itemsize = quantum * qset;
+	int item, s_pos, q_pos, rest;
+
+	if (down_interruptible(&dev->sem))
+		return -ERESTARTSYS;
+	if (*f_pos >= dev->size)
+		goto out;
+	if (*f_pos + count > dev->size)
+		count = dev->size - *f_pos;
+
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum; q_pos = rest % quantum;
+
+	dptr = scull_follow(dev, item);
+
+	if (dptr == NULL || !dptr->data || !dptr->data[s_pos])
+		goto out;
+
+	if (count > quantum - q_pos)
+		count = quantum - q_pos;
+
+	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)){
+		return -EFAULT;
+		goto out;
+	}
+
+	*f_pos += count;
+	retval = count;
+
+	out:
+		up(&dev->sem);
+		return retval;
+}
+
+ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos){
+	printk(KERN_INFO "WORKS\n");
+	struct scull_dev *dev = filp->private_data;
+	struct scull_qset *dptr;
+	int quantum = dev->quantum, qset = dev->qset;
+	int itemsize = quantum * qset;
+	int item, s_pos, q_pos, rest;
+	ssize_t retval = -ENOMEM;
+
+	if (down_interruptible(&dev->sem))
+		return -ERESTARTSYS;
+
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum; q_pos = rest % quantum;
+
+	printk(KERN_INFO "WORKS 2\n");
+	dptr = scull_follow(dev, item);
+	if (dptr == NULL)
+		goto out;
+	printk(KERN_INFO "WORKS 3\n");
+
+	if (!dptr->data){
+		dptr->data = kmalloc(sizeof(char *)  * dev->qset, GFP_KERNEL);
+		if (!dptr->data)
+			goto out;
+		memset(dptr->data, 0, qset * sizeof(char *));
+	}
+	if (!dptr->data[s_pos]){
+		dptr->data[s_pos] = kmalloc(dev->quantum, GFP_KERNEL);
+		if (!dptr->data[s_pos])
+			goto out;
+	}
+
+	if (count > quantum - q_pos)
+		count = quantum - q_pos;
+
+	if (copy_from_user(dptr->data[s_pos] + q_pos, buf, count)){
+		retval = -EFAULT;
+		goto out;
+	}
+
+	*f_pos += count;
+	retval = count;
+
+	if (dev->size < *f_pos)
+		dev->size = *f_pos;
+
+	out:
+		up(&dev->sem);
+		return retval; 
+}
+
 static struct scull_dev scull_devp_array[SCULL_DEV_NO];
 
 struct file_operations scull_fops = {
 	.owner = THIS_MODULE,
 	.open = scull_open,
-	.release = scull_release
+	.release = scull_release,
+	.read = scull_read,
+	.write = scull_write
 };
 
 static void scull_setup_cdev(struct scull_dev *dev, int index){
 	dev_t dev_no = MKDEV(scull_major, scull_minor + index);
 	cdev_init(&dev->cdev, &scull_fops);
+	dev->cdev.owner = THIS_MODULE;
 	printk(KERN_INFO "scull: Initialized Major: %d, Minor: %d\n", scull_major, scull_minor + index);
 
 	int result = cdev_add(&dev->cdev, dev_no, 1);
